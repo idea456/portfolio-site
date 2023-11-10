83a98c72cbf77794a10bdf36228ffa78b6ed70a0
Adrienne Rio Wongso Atmojo <aatm0001@student.monash.edu>
2023-11-10 16:52:00 +0800

    chore: backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Suspense, useEffect, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Scene from "./components/Room";
+import {
+    OrbitControls,
+    PresentationControls,
+    CameraControls,
+    Bounds,
+} from "@react-three/drei";
+import { Room } from "./components/Room";
 import { Typewriter } from "./components/Typewriter";
 import { Loader } from "./components/Loader";
+import gsap from "gsap";
+import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
+import * as THREE from "three";
 
 export default function App() {
-    const scroll = useRef(0);
+    const orbitRef = useRef<OrbitControlsImpl | null>(null);
+    const [isCameraEnabled, setIsCameraEnabled] = useState(true);
+    const tlRef = useRef<GSAPTimeline | null>(null);
 
     useEffect(() => {
         window.addEventListener("keydown", (e) => {
@@ -14,15 +24,28 @@ export default function App() {
                 e.preventDefault();
             }
         });
+
+        tlRef.current = gsap.timeline({});
+
+        // if (orbitRef?.current?.rotation) {
+        //     tlRef.current.to(orbitRef?.current.rotation, {
+        //         y: -90.2,
+        //         duration: 4,
+        //         onComplete: () => {
+        //             // orbitRef?.current?.enabled = false;
+        //         },
+        //     });
+        // }
     }, []);
 
     return (
         <Suspense fallback={<Loader />}>
             <div
                 style={{
-                    height: "100vh",
+                    height: "100.1vh",
                     width: "100vw",
                 }}
+                onScroll={() => console.log("lmao")}
             >
                 <div
                     style={{
@@ -33,24 +56,52 @@ export default function App() {
                         display: "block",
                     }}
                 >
-                    <Typewriter>
-                        <span style={{ color: "#FF8E3C" }}>
-                            Frontend developer,
-                        </span>{" "}
-                        Adrienne Rio
-                    </Typewriter>
+                    <Typewriter>Frontend developer, Adrienne Rio</Typewriter>
                 </div>
 
-                <Canvas shadows flat linear>
-                    <Scene scroll={scroll} />
-                    <OrbitControls
+                <Canvas
+                    shadows
+                    flat
+                    linear
+                    camera={{
+                        near: 1,
+                        far: 10000,
+                        position: [-1000, 200, 0],
+                        // position: [-100, 100, 100],
+                        // rotation: [0, Math.PI + 0.7, 0],
+                        scale: 1,
+                        fov: 50,
+                    }}
+                    style={{
+                        touchAction: "none",
+                    }}
+                >
+                    <color attach='background' args={["#f5eedf"]} />
+                    <PresentationControls
+                        snap
+                        global
+                        zoom={0.7}
+                        rotation={[0, Math.PI + 0.7, 0]}
+                        polar={[0, 0]}
+                        azimuth={[Math.PI, Math.PI * 1.65]}
+                    >
+                        <Room />
+                        {/* <OrbitControls
+                        enableDamping
+                        dampingFactor={0.1}
                         minPolarAngle={0}
-                        maxPolarAngle={1}
+                        maxPolarAngle={1.5}
                         minAzimuthAngle={Math.PI}
-                        maxAzimuthAngle={Math.PI * 1.55}
-                        minDistance={200}
+                        maxAzimuthAngle={Math.PI * 1.65}
+                        minDistance={0}
                         maxDistance={1000}
-                    />
+                        target={[0, 125, -50]}
+                        ref={orbitRef}
+                        makeDefault
+                    /> */}
+                    </PresentationControls>
+                    <axesHelper args={[200]} />
+                    {/* </Bounds> */}
                 </Canvas>
             </div>
         </Suspense>
--- src/components/Typewriter/Typewriter.tsx
+++ src/components/Typewriter/Typewriter.tsx
@@ -1,13 +1,54 @@
 import React from "react";
-import "./Typwriter.scss";
+import styled, { keyframes } from "styled-components";
+// import "./Typwriter.scss";
+
+// .typewriter {
+//     display: inline-block;
+//     width: 400px;
+//     height: fit-content;
+
+//     &__text {
+//         color: #0000;
+//         cursor: default;
+//         font-size: 60px;
+//         letter-spacing: 0.1px;
+//         background: linear-gradient(-90deg, white 5px, #0000 0) 10px 0,
+//             linear-gradient(white 0 0) 0 0;
+//         background-size: calc(32 * 1ch) 200%;
+//         -webkit-background-clip: padding-box, text;
+//         background-clip: padding-box, text;
+//         background-repeat: no-repeat;
+//         animation: b 0.7s infinite steps(1),
+//             t calc(32 * 0.1s) steps(32) forwards;
+//     }
+
+const TypewriterWrapper = styled.div`
+    display: inline-block;
+    width: 400px;
+    height: fit-content;
+`;
+
+const TypewriterText = styled.span`
+    color: #0000;
+    cursor: default;
+    font-size: 60px;
+    letter-spacing: 0.1px;
+    background: linear-gradient(-90deg, #ff8e3c 5px, #0000 0) 10px 0,
+        linear-gradient(#ff8e3c 0 0) 0 0;
+    background-size: calc(32 * 1ch) 200%;
+    -webkit-background-clip: padding-box, text;
+    background-clip: padding-box, text;
+    background-repeat: no-repeat;
+    animation: b 0.7s infinite steps(1), t calc(32 * 0.1s) steps(32) forwards;
+`;
 
 const Typewriter: React.FC<React.PropsWithChildren<unknown>> = ({
     children,
 }) => {
     return (
-        <div className='typewriter'>
-            <span className='typewriter__text'>{children}</span>
-        </div>
+        <TypewriterWrapper>
+            <TypewriterText>{children}</TypewriterText>
+        </TypewriterWrapper>
     );
 };
 
