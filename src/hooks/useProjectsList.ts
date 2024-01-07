1b2b988fb09f942e7c71a3768fc37f256f370fa1
Adrienne Rio Wongso Atmojo <aatm0001@student.monash.edu>
2024-01-07 09:53:02 +0800

    chore: retry deployment

--- src/hooks/useProjectsList.ts
+++ src/hooks/useProjectsList.ts
@@ -1,6 +1,6 @@
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
 
 const SPACE_ID = import.meta.env.CONTENTFUL_SPACE_ID;
 const TOKEN = import.meta.env.CONTENTFUL_TOKEN;
